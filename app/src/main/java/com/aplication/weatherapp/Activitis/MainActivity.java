810ab1df1af5253917416c563e7394b1c25587b0
TrungLam <huynhtrunglam167@gmail.com>
2024-02-11 12:15:05 +0700

    Backend Viewholder

--- app/src/main/java/com/aplication/weatherapp/Activitis/MainActivity.java
+++ app/src/main/java/com/aplication/weatherapp/Activitis/MainActivity.java
@@ -1,16 +1,41 @@
 package com.aplication.weatherapp.Activitis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.aplication.weatherapp.Adapter.HourlyAdapters;
+import com.aplication.weatherapp.Domains.Hourly;
 import com.aplication.weatherapp.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapterHourly;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecyclerview();
+    }
+
+    private void initRecyclerview() {
+        ArrayList<Hourly> items = new ArrayList<>();
+
+        items.add(new Hourly("9 pm", 28, "cloudy"));
+        items.add(new Hourly("11 pm", 29, "sunny"));
+        items.add(new Hourly("12 pm", 30, "wind"));
+        items.add(new Hourly("1 am", 29, "rainy"));
+        items.add(new Hourly("2 am", 27, "storm"));
+
+        recyclerView = findViewById(R.id.view1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        adapterHourly = new HourlyAdapters(items);
+        recyclerView.setAdapter(adapterHourly);
     }
 }
