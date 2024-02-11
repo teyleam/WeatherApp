921b1fb7e97c22f8b62c67eaa4a8486826bc3194
TrungLam <huynhtrunglam167@gmail.com>
2024-02-11 12:03:54 +0700

    UI viewholder

--- app/src/main/java/com/aplication/weatherapp/Adapter/HourlyAdapters.java
+++ app/src/main/java/com/aplication/weatherapp/Adapter/HourlyAdapters.java
@@ -1,5 +1,7 @@
 package com.aplication.weatherapp.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,23 +11,43 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.aplication.weatherapp.Domains.Hourly;
+import com.aplication.weatherapp.R;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 public class HourlyAdapters extends RecyclerView.Adapter<HourlyAdapters.viewHolder> {
+    ArrayList<Hourly> items;
+    Context context;
+
+    public HourlyAdapters(ArrayList<Hourly> items) {
+        this.items = items;
+    }
 
     @NonNull
     @Override
     public HourlyAdapters.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_hourly, parent, false);
+        context = parent.getContext();
+        return new viewHolder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HourlyAdapters.viewHolder holder, int position) {
+    holder.hourTxt.setText(items.get(position).getHour());
+    holder.tempTxt.setText(items.get(position).getTemp()+ "°");
 
+    int drawableResourceId = holder.itemView.getResources().getIdentifier(items.get(position).getPicPath(),
+            "drawable",holder.itemView.getContext().getPackageName());
+
+        Glide.with(context)
+                .load(drawableResourceId)
+                .into(holder.pic);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     public class viewHolder extends RecyclerView.ViewHolder {
@@ -34,7 +56,9 @@ public class HourlyAdapters extends RecyclerView.Adapter<HourlyAdapters.viewHold
         public viewHolder(@NonNull View itemView) {
             super(itemView);
 
-            hourTxt = itemView.findViewById(R.id.)
+            hourTxt = itemView.findViewById(R.id.hourTxt);
+            tempTxt = itemView.findViewById(R.id.tempTxt);
+            pic = itemView.findViewById(R.id.pic);
         }
     }
 }
