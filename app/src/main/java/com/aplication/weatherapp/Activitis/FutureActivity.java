2f5663fa0af2b8a5eabd4914c994d44cf7c16482
TrungLam <huynhtrunglam167@gmail.com>
2024-02-16 19:59:15 +0700

    update fuction setVariable

--- app/src/main/java/com/aplication/weatherapp/Activitis/FutureActivity.java
+++ app/src/main/java/com/aplication/weatherapp/Activitis/FutureActivity.java
@@ -1,10 +1,14 @@
 package com.aplication.weatherapp.Activitis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.aplication.weatherapp.Adapter.FutureAdapters;
 import com.aplication.weatherapp.Domains.FutureDomain;
@@ -26,6 +30,13 @@ public class FutureActivity extends AppCompatActivity {
     }
 
     private void setVariable() {
+        View backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FutureActivity.this, MainActivity.class));
+            }
+        });
     }
 
     private void initRecyclerView() {
