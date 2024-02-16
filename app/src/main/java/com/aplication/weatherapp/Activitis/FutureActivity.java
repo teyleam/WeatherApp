5dd3b7294c4a40356e9ee2d7b2d7cedfa20dee68
TrungLam <huynhtrunglam167@gmail.com>
2024-02-16 19:48:10 +0700

    update backend FutureActivity

--- app/src/main/java/com/aplication/weatherapp/Activitis/FutureActivity.java
+++ app/src/main/java/com/aplication/weatherapp/Activitis/FutureActivity.java
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.aplication.weatherapp.Domains.FutureDomain;
 import com.aplication.weatherapp.R;
+
+import java.util.ArrayList;
 
 public class FutureActivity extends AppCompatActivity {
     
@@ -20,5 +23,14 @@ public class FutureActivity extends AppCompatActivity {
     }
 
     private void initRecyclerView() {
+        ArrayList<FutureDomain> items = new ArrayList<>();
+
+        items.add(new FutureDomain("Sat", "storm","storm",20,10));
+        items.add(new FutureDomain("Sun", "cloudy","cloudy",25,16));
+        items.add(new FutureDomain("Mon", "windy","windy",29,14));
+        items.add(new FutureDomain("Tue", "cloudy_sunny","Cloudy_Sunny",21,19));
+        items.add(new FutureDomain("Wen", "sun","Sunny",28,10));
+        items.add(new FutureDomain("Thu", "rainy","Rainy",23,1));
+
     }
 }
