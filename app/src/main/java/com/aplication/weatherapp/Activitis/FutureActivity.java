c9763e4f0bcb0cb42ef7baa77a28c1fe93a92b87
TrungLam <huynhtrunglam167@gmail.com>
2024-02-16 19:42:34 +0700

    backend FutureActivity

--- app/src/main/java/com/aplication/weatherapp/Activitis/FutureActivity.java
+++ app/src/main/java/com/aplication/weatherapp/Activitis/FutureActivity.java
@@ -1,16 +1,24 @@
 package com.aplication.weatherapp.Activitis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.aplication.weatherapp.R;
 
 public class FutureActivity extends AppCompatActivity {
-
+    
+    private RecyclerView adapterTommorow;
+    public RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_future);
+        
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
     }
 }
