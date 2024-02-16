323dadbe86305af2de8b5d30938b1185922d23de
TrungLam <huynhtrunglam167@gmail.com>
2024-02-16 19:19:12 +0700

    backend FutureAdapters

---
+++ app/src/main/java/com/aplication/weatherapp/Adapter/FutureAdapters.java
@@ -0,0 +1,64 @@
+package com.aplication.weatherapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aplication.weatherapp.Domains.Hourly;
+import com.aplication.weatherapp.R;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class FutureAdapters extends RecyclerView.Adapter<FutureAdapters.viewHolder> {
+    ArrayList<Hourly> items;
+    Context context;
+
+    public FutureAdapters(ArrayList<Hourly> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public FutureAdapters.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_hourly, parent, false);
+        context = parent.getContext();
+        return new viewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FutureAdapters.viewHolder holder, int position) {
+    holder.hourTxt.setText(items.get(position).getHour());
+    holder.tempTxt.setText(items.get(position).getTemp()+ "°");
+
+    int drawableResourceId = holder.itemView.getResources().getIdentifier(items.get(position).getPicPath(),
+            "drawable",holder.itemView.getContext().getPackageName());
+
+        Glide.with(context)
+                .load(drawableResourceId)
+                .into(holder.pic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        TextView hourTxt, tempTxt;
+        ImageView pic;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            hourTxt = itemView.findViewById(R.id.hourTxt);
+            tempTxt = itemView.findViewById(R.id.tempTxt);
+            pic = itemView.findViewById(R.id.pic);
+        }
+    }
+}
